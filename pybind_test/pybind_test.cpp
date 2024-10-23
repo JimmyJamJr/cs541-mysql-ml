a73e553b2ab50d7bd55e2c40df13bcd02f4889fc
Jimson Huang <jimsonjy@gmail.com>
2024-10-23 16:44:27 -0400

    Included code to print out the pytorch version in C++

--- pybind_test/pybind_test.cpp
+++ pybind_test/pybind_test.cpp
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h>
 #include <iostream>
+#include <string>
 
 namespace py = pybind11;
 
@@ -19,6 +20,11 @@ int main(int argc, char *argv[]) {
     double cpp_result = result.cast<double>();
 
     std::cout << "The square root of 16 is: " << cpp_result << std::endl;
+
+    py::module_ torch = py::module_::import("torch");
+    py::object torch_version = torch.attr("__version__");
+    std::string torch_version_cpp = torch_version.cast<std::string>();
+    std::cout << "PyTorch Version: " << torch_version_cpp << std::endl;
         
     return 0;
 }

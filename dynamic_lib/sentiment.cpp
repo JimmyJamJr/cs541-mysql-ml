33e2287929820d5e724ad15d36b35dea3a68b854
Jimson Huang <jimsonjy@gmail.com>
2024-11-18 15:44:31 -0500

    Removed some comments

--- dynamic_lib/sentiment.cpp
+++ dynamic_lib/sentiment.cpp
@@ -56,7 +56,6 @@ bool sentiment_init(UDF_INIT * initid, UDF_ARGS *args, char * message) {
             py::module_ os = py::module_::import("os");
             py::object chdir = os.attr("chdir");
             chdir("../lib/plugin");
-//            chdir("/Users/jimmy/Documents/Python/cs541-mysql-ml/dynamic_lib/");
 
             sent = py::module_::import("sentiment_analysis_function");
         });
@@ -67,24 +66,6 @@ bool sentiment_init(UDF_INIT * initid, UDF_ARGS *args, char * message) {
         return 1; // Failure
     }
 
-//    py::module_ os = py::module_::import("os");
-//    py::object getcwd = os.attr("getcwd");
-//    py::object cwd_result = getcwd();
-//    std::string cwd_result_cpp = cwd_result.cast<std::string>();
-
-//    py::gil_scoped_acquire acquire;
-//
-//    auto sent = py::module_::import("sentiment_analysis_function");
-//    auto sentiment_analysis_func = sent.attr("sentiment_analysis");
-////
-//    py::object test_func = sent.attr("test");
-//    py::object sentiment_result = test_func();
-//    std::string sentiment_result_cpp = sentiment_result.cast<std::string>();
-//    strcpy(message, sentiment_result_cpp.c_str());
-
-//    strcpy(message, cwd_result_cpp.c_str());
-//    return 1;
-//    strcpy(message, "test2");
     // Allocate memory for the result
     initid->ptr = (char *)malloc(16384); // Adjust size as needed
     if (!initid->ptr) {
@@ -123,7 +104,6 @@ char * sentiment(UDF_INIT * initid, UDF_ARGS *args, char *result, unsigned long 
         char *res_ptr = (char *)initid->ptr;
         strcpy(res_ptr, sentiment_result_cpp.c_str());
         *length = sentiment_result_cpp.length();
-//        *length = input_value.length();
         return res_ptr;
     }
     catch (const py::error_already_set& e) {
@@ -131,42 +111,4 @@ char * sentiment(UDF_INIT * initid, UDF_ARGS *args, char *result, unsigned long 
         *is_null = 1;
         return nullptr;
     }
-
-    //     Test with sentiment test function
-//    try {
-//        py::gil_scoped_acquire acquire; // Acquire the GIL
-////        auto sent = py::module_::import("sentiment_analysis_function");
-//        py::object test_func = sent.attr("test");
-//        py::object sentiment_result = test_func();
-//        std::string sentiment_result_cpp = sentiment_result.cast<std::string>();
-//
-//        char *res_ptr = (char *)initid->ptr;
-//        strcpy(res_ptr, sentiment_result_cpp.c_str());
-//        *length = sentiment_result_cpp.length();
-//        return res_ptr;
-//    } catch (const py::error_already_set& e) {
-//        strcpy(error, e.what());
-//        *is_null = 1;
-//        return nullptr;
-//    }
-
-// Test with hello world
-//    try {
-//        py::gil_scoped_acquire acquire;
-//
-//        py::object py_result = py::eval("\"Hello from Python\"");
-//        std::string result_str = py::str(py_result).cast<std::string>();
-//
-//        // Use initid->ptr for the result as before...
-//        char *res_ptr = (char *)initid->ptr;
-//        strcpy(res_ptr, result_str.c_str());
-//        *length = result_str.length();
-//        return res_ptr;
-//
-//    }
-//    catch (const py::error_already_set& e) {
-//        strncpy(error, e.what(), MYSQL_ERRMSG_SIZE);
-//        error[MYSQL_ERRMSG_SIZE - 1] = '\0';
-//        *is_null = 1;
-//        return nullptr;
 }
